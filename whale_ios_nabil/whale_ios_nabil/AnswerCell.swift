61545eccc1fec8b83a68e6e718941a9a224ac909
nkhaja <nbl.khaja@gmail.com>
2017-03-30 11:58:23 -0700

    setup most networking architecture, working on creating answer cell, then pagination

---
+++ whale_ios_nabil/whale_ios_nabil/AnswerCell.swift
@@ -0,0 +1,13 @@
+//
+//  AnswerCell.swift
+//  whale_ios_nabil
+//
+//  Created by Nabil K on 2017-03-30.
+//  Copyright © 2017 MakeSchool. All rights reserved.
+//
+
+import UIKit
+
+class AnswerCell: UICollectionViewCell {
+    
+}
---
+++ whale_ios_nabil/whale_ios_nabil/SignUpController.swift
@@ -0,0 +1,35 @@
+//
+//  SignUpController.swift
+//  whale_ios_nabil
+//
+//  Created by Nabil K on 2017-03-29.
+//  Copyright © 2017 MakeSchool. All rights reserved.
+//
+
+import UIKit
+
+class SignUpController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
